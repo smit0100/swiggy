cbb9d5836d12f1e39790947f9006dc02ee8d1ee6
poojansakhreliya8050 <poojasakhreliya12@gmail.com>
2023-03-12 16:44:40 +0530

    added

--- frontend/src/Review/DeliveryReview.js
+++ frontend/src/Review/DeliveryReview.js
@@ -3,38 +3,26 @@ import { MdOutlineRateReview } from 'react-icons/md'
 import swal from 'sweetalert'
 
 
-const ReviewPopUp = () => {
+const ReviewPopUp = ({setDeliveryReview}) => {
 
   const [review, setReview] = useState('')
   const [rating, setRating] = useState(0);
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(true);
   console.log("Rating point",rating);
   const handleRatingChange = (newRating) => {
     setRating(newRating);
   };
 console.log("Review text : ",review,"Rating",rating);
   return (
-    <div className="w-full sm:w-[400px] flex pt-5">
-      <div className="shadow-black shadow-sm bg-white p-3">
-        <MdOutlineRateReview className='text-xl' />
-        <div className="py-2">
-          <p className="font-semibold text-xl">Write Your Review</p>
-          <p className="w-10/12 font-[350] text-slate-500"></p>
-          <button className="inline-block mt-7 bg-white hover:text-white hover:bg-blue-600 -bottom-4 font-bold  rounded border border-current px-8 py-[6px] text-xs uppercase  text-blue-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-blue-500" type="button"
-            onClick={() => setShowModal(true)}>
-            Review
-          </button>
-        </div>
-
-        {showModal ? (
+   
           <>
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
               <div className="relative w-auto my-6 mx-auto max-w-3xl">
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <h3 className="text-xl font-semibold">Write Your Review</h3>
+                    <h3 className="text-xl font-semibold">Write Your Review for Delivery Boy</h3>
                     <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={() => setShowModal(false)}>
+                      onClick={() => setDeliveryReview(false)}>
                       <span className=" text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
                         X
                       </span>
@@ -56,7 +44,7 @@ console.log("Review text : ",review,"Rating",rating);
                   {/*footer*/}
                   <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                     <button className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button"
-                      onClick={() => setShowModal(false)}>
+                      onClick={() => setDeliveryReview(false)}>
                       Close
                     </button>
                     <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button"
@@ -69,9 +57,7 @@ console.log("Review text : ",review,"Rating",rating);
             </div>
             <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
           </>
-        ) : null}
-      </div>
-    </div>
+    
   )
 }
 
