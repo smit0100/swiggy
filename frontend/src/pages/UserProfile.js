41029204340106530e83e5bb9854b0512c462cdd
dixiit <dixitpadsala628@gmail.com>
2023-03-29 12:11:29 +0530

    someThing Change

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import UserAddress from "../components/UserAddress";
 import ChangePasswordPopup from "../components/ChangePasswordPopup";
 import UpdateProfileDetails from "../components/UpdateProfileDetails";
 import axios from "axios";
-import dateFormat from "dateformat";
-
-import { BsArrowRightCircle } from "react-icons/bs";
 
 import { useCookies } from "react-cookie";
 import { userData } from "../redux/user/userSlice";
-import swal from "sweetalert";
-import { AiTwotoneStar } from "react-icons/ai";
+import swal from "sweetalert"
+
+import OrderDetailsCard from "../components/OrderDetailsCard";
 
 const UserProfile = () => {
   // for popup state
@@ -90,36 +88,16 @@ const UserProfile = () => {
         <div className="row ">
           <div className="w-full sm:w-1/5 p-5">
             <ul className="space-y-3">
-              <li
-                className="text-lg border-b-2 cursor-pointer"
-                onClick={() => setOpenTab(1)}
-              >
-                Profile
-              </li>
-              {/* <li className='text-lg border-b-2 cursor-pointer' onClick={() => navigate("/orderDetails")}>Order Detail</li> */}
-              <li
-                className="text-lg border-b-2 cursor-pointer"
-                onClick={() => setOpenTab(2)}
-              >
-                Your Orders
-              </li>
-              <li className="text-lg border-b-2 cursor-pointer">
-                <button
-                  onClick={logout}
-                  className="inline-block bg-inherit hover:text-white hover:bg-red-600 font-bold  rounded  px-4  py-[6px] text-xs uppercase  text-red-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-red-500"
-                >
-                  Logout
-                </button>
-              </li>
+            <li className="text-lg border-b-2 cursor-pointer" onClick={() => setOpenTab(1)} > Profile </li>
+            <li className="text-lg border-b-2 cursor-pointer" onClick={() => setOpenTab(2)} > Your Orders </li>
+            <li className="text-lg border-b-2 cursor-pointer">
+              <button onClick={logout} className="inline-block bg-inherit hover:text-white hover:bg-red-600 font-bold rounded px-4 py-[6px] text-xs uppercase text-red-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-red-500" > Logout </button>
+            </li>
             </ul>
           </div>
 
           {/* profile module  */}
-          <div
-            className={`${
-              openTab === 1 ? "block" : "hidden"
-            } w-full sm:w-4/5 p-5`}
-          >
+          <div className={`${openTab === 1 ? "block" : "hidden" } w-full sm:w-4/5 p-5`} >
             <h1 className="text-xl font-semibold pb-5 capitalize">
               your profile
             </h1>
@@ -143,21 +121,8 @@ const UserProfile = () => {
                     {user != null && user.email}
                   </li>
                   <li className="py-3 text-lg font-normal">
-                    <button
-                      type="button"
-                      onClick={() => setChangePassword(true)}
-                      id="password"
-                      value="Change Password"
-                      className="inline-block mr-2 bg-inherit hover:text-white hover:bg-blue-400 -bottom-4 font-bold  rounded border border-current px-4 py-[6px] text-xs uppercase  text-blue-400 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-blue-400"
-                    >
-                      <i className="fas fa-repeat"></i> Change Password
-                    </button>
-                    <button
-                      onClick={() => setupdateProfile(true)}
-                      className="inline-block bg-inherit hover:text-white hover:bg-blue-600 -bottom-4 font-bold  rounded border border-current px-8 py-[6px] text-xs uppercase  text-blue-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-blue-500"
-                    >
-                      Update Profile{" "}
-                    </button>
+                  <button type="button" onClick={() => setChangePassword(true)} id="password" value="Change Password" className="inline-block mr-2 bg-inherit hover:text-white hover:bg-blue-400 -bottom-4 font-bold rounded border border-current px-4 py-[6px] text-xs uppercase text-blue-400 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-blue-400" > <i className="fas fa-repeat"></i> Change Password </button> 
+                  <button onClick={() => setupdateProfile(true)} className="inline-block bg-inherit hover:text-white hover:bg-blue-600 -bottom-4 font-bold rounded border border-current px-8 py-[6px] text-xs uppercase text-blue-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-blue-500" > Update Profile{" "} </button>
                   </li>
                 </ul>
               </div>
@@ -170,19 +135,15 @@ const UserProfile = () => {
             <UserAddress />
           </div>
           {/* order module  */}
-          <div
-            className={`${
-              openTab === 2 ? "block" : "hidden"
-            } w-full sm:w-4/5 p-5`}
-          >
+          <div className={`${openTab === 2 ? "block" : "hidden"} w-full sm:w-4/5 p-5`}>
             <h1 className="text-xl font-semibold pb-5 capitalize">
               Order Detail
             </h1>
             <div className="w-full flex flex-wrap  gap-2 justify-evenly ">
               {order
                 ? order.map((item) => {
-                    return <OrderDetailsCard items={item} />;
-                  })
+                  return <OrderDetailsCard items={item} />;
+                })
                 : ""}
             </div>
           </div>
@@ -204,65 +165,3 @@ const UserProfile = () => {
 
 export default UserProfile;
 
-export const OrderDetailsCard = ({ items }) => {
-  console.log(items);
-  let bgReview = "bg-green-600";
-
-  if (items != null) {
-    if (+items.resturant.rating <= 1.5) {
-      bgReview = "bg-red-600";
-    } else if (+items.resturant.rating > 1.5 && items.resturant.rating < 3.5) {
-      bgReview = "bg-orange-600";
-    } else {
-      bgReview = "bg-green-600";
-    }
-  }
-  return (
-    // <div>
-    // <Link  to={`/orderDetails/${items._id}`}>{items._id}</Link>
-    // </div>
-    <div className="flex w-[32%] relative  text-gray-900 antialiased">
-      <div>
-        <div className="overflow-hidden w-full   rounded-lg">
-          <img
-            src={items?.resturant?.bgImageUrl[0]}
-            alt=" random imgee"
-            className=" w-full rounded-lg object-cover object-center hover:scale-110 shadow-md transition-all duration-300"
-          />
-        </div>
-        <Link to={`/orderDetails/${items._id}`}>
-          <div className="relative group -mt-16 px-4 hover:skew-x-1 transition-all duration-500">
-            <div className="rounded-lg bg-orange-200 p-5 shadow-lg">
-              <div className="flex items-baseline">
-                <div className="text-xs font-semibold uppercase tracking-wider text-gray-600">
-                  {dateFormat(items.createdAt, "dd")} &bull;{" "}
-                  {dateFormat(items.createdAt, "mm")} &bull;{" "}
-                  {dateFormat(items.createdAt, "yyyy")}
-                </div>
-              </div>
-              <h4 className="mt-1 truncate text-xl font-semibold uppercase leading-tight">
-                {items?.resturant?.name}
-              </h4>
-              <div className="mt-1 font-medium">
-                ₹{items?.total}
-                <span className="text-sm text-gray-600"> /Total Amout</span>
-              </div>
-              <div className="mt-4 flex justify-between items-center">
-                <span className="text-md font-semibold text-teal-600">
-                  <div
-                    className={` ${bgReview} py-1 my-2  px-3 bg-green-600 flex items-center w-fit rounded-md text-white font-bold `}
-                  >
-                    {items != null && items.resturant.rating} &nbsp;{" "}
-                    <AiTwotoneStar />
-                  </div>
-                </span>
-                <BsArrowRightCircle className="text-xl group-hover:translate-x-3 transition-all duration-500" />
-              </div>
-            </div>
-          </div>
-        </Link>
-        {/* <div className='hidden'><CustomerOrderCard items={items}/></div>   */}
-      </div>
-    </div>
-  );
-};
--- owner/src/components/OrderDetail.js
+++ owner/src/components/OrderDetail.js
@@ -10,10 +10,8 @@ const OrderDetail = () => {
   const [order, setOrder] = useState([])
   const owner = useSelector(state => state.userData.user)
   const navigate = useNavigate()
-  const [selectedOrderId, setSelectedOrderId] = useState(null);
-  const [showOrderDetail, setShowOrderDetail] = useState(false)
-  const [orderStatus, setOrderStatus] = useState('not define')
-
+  const [filter, setFilter] = useState('All');
+  const [filteredItems, setFilteredItems] = useState([]);
 
   useEffect(() => {
     (
@@ -23,30 +21,26 @@ const OrderDetail = () => {
         const response = await axios.get(`http://localhost:4000/resturant/allResturantOrder?id=${owner._id}`)
         console.log(response.data.order);
         setOrder(response.data.order);
+        setFilteredItems(response.data.order)
         setIsLoading(false)
-
-
 
       }
     )();
   }, [])
 
+  useEffect(() => {
+    if (filter === 'All') {
+      setFilteredItems(order);
+    } else {
+      const filtered = order.filter(item => item.status === filter);
+      setFilteredItems(filtered);
+    }
+  }, [filter]);
 
-  const orders = [
-    { id: 123, customerName: "John Doe", totalPrice: 45.99, orderDate: "2023-02-21", status: "Delivered" },
-    { id: 124, customerName: "Jane Smith", totalPrice: 26.50, orderDate: "2023-02-20", status: "Preparing" },
-    { id: 164, customerName: "Jani Mohan", totalPrice: 65.00, orderDate: "2023-05-05", status: "On the way" },];
-
-  // const handleRowClick = (orderId) => {
-  //   setSelectedOrderId(orderId);
-  //   navigate(`/ordersummary/:${selectedOrderId}`)
-  //   // Fetch the order details using the order ID
-  //   // Display the quick review details in a modal or tooltip
-  // };
   return (
     <>
       {
-        isLoading ? <Loader />: <>
+        isLoading ? <Loader /> : <>
           {
             <div className='mx-5'>
               <div className='p-5 shadow-md'>
@@ -59,23 +53,31 @@ const OrderDetail = () => {
                         <th className="py-3 px-6 text-left">Customer Name</th>
                         <th className="py-3 px-6 text-left">Total Price</th>
                         <th className="py-3 px-6 text-left">Order Date</th>
-                        <th className="py-3 px-6 text-left">Status</th>
+                        <th className="py-3 px-6 text-left">
+                          <select className='outline-none border-none bg-inherit border-b-2 border-black' value={filter} onChange={(e) => setFilter(e.target.value)}>
+                            <option value="All">All</option>
+                            {/* <option value="process">Process</option> */}
+                            <option value="accept">Accept</option>
+                            <option value="on the way">On the way</option>
+                            <option value="delivered">Delivered</option>
+                          </select>
+                        </th>
                       </tr>
                     </thead>
 
                     <tbody className="text-gray-600 text-sm font-light">
-                      {order && order.map((order) => (
+                      {order && filteredItems.map((order) => (
                         <tr
                           key={order && order._id}
                           //  ${selectedOrderId === order._id ? "bg-blue-100" : ""}`
                           className={`border-b border-gray-200 hover:bg-gray-100`}
-                          onClick={() => { navigate('/ordersummary', { state: order._id }); setSelectedOrderId(order.id) }}>
-                          <td className="py-3 px-6 text-left">{order._id}</td>
-                          {/* <td className="py-3 px-6 text-left">{order.customer.name}</td> */}
-                          <td className="py-3 px-6 text-left">₹{order.total}</td>
+                          onClick={() => { navigate('/ordersummary', { state: order._id }); }}>
+                          <td className="py-3 px-6 text-left">{order !== null && order._id}</td>
+                          <td className="py-3 px-6 text-left">{order !== null && order.customer?.name}</td>
+                          <td className="py-3 px-6 text-left">₹{order !== null && order?.total}</td>
                           <td className="py-3 px-6 text-left">{dateFormat(order.createdAt, "mmmm dS, yyyy")}</td>
                           <td className="py-3 px-6 text-left">
-                            <span className={`py-1 px-3 rounded-full text-xs ${order.status === "Delivered" ? "bg-green-200 text-green-600" : order.status === "Preparing" ? "bg-yellow-200 text-yellow-600" : "bg-amber-200 text-amber-600"}`}>
+                            <span className={`py-1 px-3 rounded-full text-xs ${order !== null && order?.status === "delivered" ? "bg-green-200 text-green-600" : order.status === "accept" ? "bg-orange-200 text-orange-600" : "bg-amber-200 text-amber-600"}`}>
                               {order.status}
                             </span>
                           </td>
@@ -84,90 +86,6 @@ const OrderDetail = () => {
                     </tbody>
                   </table>
                 </div>
-
-
-
-                {/* {
-                  showOrderDetail &&
-                  <div className='absolute bg-red-500 top-0 left-0 z-50'>
-                    <div className="fixed z-50 inset-0 overflow-y-auto">
-                      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-                          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-                        </div>
-                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-                        <div
-                          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-                          role="dialog"
-                          aria-modal="true"
-                          aria-labelledby="modal-headline">
-                          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                            <div className="sm:flex sm:items-start">
-                              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                                <svg
-                                  className="h-6 w-6 text-blue-600"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  stroke="currentColor"
-                                  aria-hidden="true"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M5 13l4 4L19 7"
-                                  />
-                                </svg>
-                              </div>
-                              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                <h3
-                                  className="text-lg leading-6 font-medium text-gray-900"
-                                  id="modal-headline">
-                                  Order Details - {selectedOrderId}
-                                </h3>
-                                {
-                                  orders.filter(order => order.id === selectedOrderId).map(order => (
-                                    <div className="mt-2">
-                                      <table>
-                                        <tr>
-                                          <td className="text-sm text-gray-500 pr-3">Customer</td>
-                                          <td>{order.customerName}</td>
-                                        </tr>
-                                        <tr>
-                                          <td className="text-sm text-gray-500 pr-3">Order Date</td>
-                                          <td>{order.orderDate}</td>
-                                        </tr>
-                                        <tr>
-                                          <td className="text-sm text-gray-500 pr-3">Total Price</td>
-                                          <td>{order.totalPrice}</td>
-                                        </tr>
-                                        <tr>
-                                          <td className="text-sm text-gray-500 pr-3">Status</td>
-                                          <td>{order.status}</td>
-                                        </tr>
-                                      </table>
-                                    </div>
-                                  ))}
-                              </div>
-                            </div>
-                          </div>
-                          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                            <button
-                              type="button"
-                              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                              onClick={() => {
-                                setShowOrderDetail(false)
-                              }}
-                            >
-                              Close
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                } */}
               </div>
             </div>
 
@@ -181,80 +99,3 @@ const OrderDetail = () => {
 }
 
 export default OrderDetail
-
-// export const FoodOrderDetailModal = ({ order, orderId }) => {
-//   return (
-//     <div className="fixed z-50 inset-0 overflow-y-auto">
-//       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-//         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-//           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-//         </div>
-
-//         <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
-//         <div
-//           className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-//           role="dialog"
-//           aria-modal="true"
-//           aria-labelledby="modal-headline"
-//         >
-//           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-//             <div className="sm:flex sm:items-start">
-//               <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-//                 <svg
-//                   className="h-6 w-6 text-blue-600"
-//                   xmlns="http://www.w3.org/2000/svg"
-//                   fill="none"
-//                   viewBox="0 0 24 24"
-//                   stroke="currentColor"
-//                   aria-hidden="true"
-//                 >
-//                   <path
-//                     strokeLinecap="round"
-//                     strokeLinejoin="round"
-//                     strokeWidth={2}
-//                     d="M5 13l4 4L19 7"
-//                   />
-//                 </svg>
-//               </div>
-//               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-//                 <h3
-//                   className="text-lg leading-6 font-medium text-gray-900"
-//                   id="modal-headline"
-//                 >
-//                   Order Details - {order.id}
-//                 </h3>
-//                 <div className="mt-2">
-//                   <p className="text-sm text-gray-500">
-//                     Customer: {order.customerName}
-//                   </p>
-//                   <p className="text-sm text-gray-500">
-//                     Order Date: {order.orderDate}
-//                   </p>
-//                   <p className="text-sm text-gray-500">
-//                     Total Price: ${order.totalPrice}
-//                   </p>
-//                   <p className="text-sm text-gray-500">
-//                     Status: {order.status}
-//                   </p>
-//                   {/* Add more details as needed */}
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-//             <button
-//               type="button"
-//               className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-//               onClick={() => {
-//                 setShowOrderDetail(false)
-//               }}
-//             >
-//               Close
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
