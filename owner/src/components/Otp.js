67d7b9a356dd914370e50a83c81d853bd02fe83a
Rohit kajavadra <krishnakajavadar6178@gmail.com>
2023-03-29 22:46:37 +0530

    owner otp added

--- owner/src/components/Otp.js
+++ owner/src/components/Otp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
@@ -7,29 +7,53 @@ import { ownerLogIn, userData } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import swal from "sweetalert";
 const Otp = () => {
-  const [otp, setOtp] = useState("");
   const [searchParams] = useSearchParams();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
   const id = searchParams.get("id");
   const email = searchParams.get("email");
+  const [otp, setOTP] = useState(["", "", "", "", "", ""]);
 
-  console.log(email);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(otp.join(""));
+  };
+
+  const handleChange = (event, index) => {
+    const value = event.target.value;
+    setOTP((prevOTP) => {
+      const newOTP = [...prevOTP];
+      newOTP[index] = value;
+      return newOTP;
+    });
+    if (value && refs[index + 1]) {
+      refs[index + 1].current.focus();
+    } else if (!value && refs[index - 1]) {
+      refs[index - 1].current.focus();
+    }
+  };
+
+  const refs = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+  ];
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
   const handleClick = async (e) => {
+
     e.preventDefault();
-    console.log(id, otp);
     try {
       const response = await axios.post(
         "http://localhost:4000/resturant/verify",
         {
           id,
-          otp,
+          otp:otp.join(""),
         }
       );
-      console.log(response.data);
+      console.log("====>>",response.data);
       dispatch(userData(response.data.rest));
       dispatch(ownerLogIn(true));
       localStorage.setItem("isOwnerLogIn", JSON.stringify(true));
@@ -51,50 +75,38 @@ const Otp = () => {
     return email.substr(0, 5) + hidden + email.substring(index);
   }
 
-  //     function OTPInput() {
-  //         const inputs = document.querySelectorAll('#otp > *[id]');
-  //         for (let i = 0; i < inputs.length; i++) {
-  //             inputs[i].addEventListener('keydown', function (event) {
-  //                 if (event.key === "Backspace") {
-  //                     inputs[i].value = '';
-  //                     if (i !== 0) inputs[i - 1].focus();
-  //                 }
-  //                 else {
-  //                     if (i === inputs.length - 1 && inputs[i].value !== '') { return true; }
-  //                     else if (event.keyCode > 47 && event.keyCode < 58) {
-  //                         inputs[i].value = event.key;
-  //                         if (i !== inputs.length - 1)
-  //                             inputs[i + 1].focus(); event.preventDefault();
-  //                     }
-  //                     else if (event.keyCode > 64 && event.keyCode < 91) {
-  //                         inputs[i].value = String.fromCharCode(event.keyCode);
-  //                         if (i !== inputs.length - 1) inputs[i + 1].focus(); event.preventDefault();
-  //                     }
-  //                 }
-  //             });
-  //         }
-  //     } OTPInput();
-  // });
-
   return (
-    <div className="h-screen bg-blue-500 py-20 px-3 ">
-      <div className="container mx-auto mt-36">
-        <div className="max-w-sm mx-auto md:max-w-lg">
+    <div className=" bg-blue-500 flex flex-col items-center justify-center min-h-screen">
+        <div className="max-w-lg mx-auto md:max-w-lg">
           <div className="w-full">
-            <div className="bg-white h-64 py-3 rounded text-center">
+            <div className="bg-white p-5 rounded-2xl text-center">
               <h1 className="text-2xl font-bold">OTP Verification</h1>
               <div className="flex flex-col mt-4">
                 <span>Enter the OTP you received at</span>
                 <span className="font-bold">{hideEmail(email)}</span>
               </div>
-              <input
-                value={otp}
-                onChange={(e) => setOtp(e.target.value)}
-                type="email"
-                className="mt-6 pl-5  py-4 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150 font-bold w-1/3 border-2 border-black"
-                placeholder="Enter your otp..."
-              />
-
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center my-5"
+              >
+                <div className="flex justify-center">
+                  {otp.map((digit, index) => (
+                    <input
+                      key={index}
+                      type="text"
+                      min="0"
+                      max="9"
+                      ref={refs[index]}
+                      className={`w-12 h-12 sm:w-16 sm:h-16 mx-2 text-center text-3xl font-bold border border-gray-400 rounded-lg`}
+                      maxLength={1}
+                      placeholder="-"
+                      value={digit}
+                      onChange={(event) => handleChange(event, index)}
+                      required
+                    />
+                  ))}
+                </div>
+              </form>
               <div className="flex justify-center text-center mt-5">
                 <a className="flex items-center text-blue-700 hover:text-blue-900 cursor-pointer">
                   <span className="font-bold">Resend OTP</span>
@@ -111,7 +123,6 @@ const Otp = () => {
             </div>
           </div>
         </div>
-      </div>
     </div>
   );
 };
