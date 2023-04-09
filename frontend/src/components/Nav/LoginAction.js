f5dfb17ff149a5bf904b4998f074d76195266628
Rohit kajavadra <krishnakajavadar6178@gmail.com>
2023-04-09 12:31:28 +0530

    Restaurant UI update in frontend

--- frontend/src/components/Nav/LoginAction.js
+++ frontend/src/components/Nav/LoginAction.js
@@ -3,16 +3,17 @@ import { MdLogin } from "react-icons/md"
 import { motion } from "framer-motion"
 
 const LoginAction = ({text,  mobile,currentColor}) => {
+  console.log("==currentColor",currentColor);
   return (
     <Link to="/login">
         <motion.div
-          className={` flex items-center gap-3 border border-slate-200 px-3 py-1 rounded-lg cursor-pointer`}
+          className={`flex items-center gap-3 border border-${currentColor == "slate-800" ? "black" : "white"} px-3 py-1 rounded-lg cursor-pointer`}
           whileTap={{ scale: 0.8 }}
           whileHover={{ scale: 1.1 }}
           
         >
           <MdLogin className={` ${mobile && 'text-2xl text-white'} text-${currentColor}`} />
-          {text && <p className={`text-${currentColor}`}>{text}</p>}
+          {text && <p className={`font-mono text-${currentColor}`}>{text}</p>}
         </motion.div>
     </Link>
   )
