ef118354e663d870e62726c0fd56cc5f624040bd
Rohit kajavadra <krishnakajavadar6178@gmail.com>
2023-04-11 09:08:33 +0530

    address modal UI in checkout

--- frontend/src/components/UserAddress.js
+++ frontend/src/components/UserAddress.js
@@ -6,6 +6,7 @@ import swal from "sweetalert";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import InlineButtonLoader from "./InlineButtonLoader";
+import { toast } from "react-toastify";
 
 const UserAddress = () => {
   const user = useSelector((state) => state.userData.user);
@@ -88,7 +89,6 @@ const UserAddress = () => {
     else setPincodeError("");
   };
 
-
   const handleSubmitForAddress = async (e) => {
     e.preventDefault();
     setIsAddLoading(true);
@@ -114,7 +114,9 @@ const UserAddress = () => {
       setCity("");
       setState("");
       setPincode("");
+      toast.success("🔥Address saved successfully.");
       setIsAddLoading(false);
+      setShowModal(false);
     } catch (err) {
       console.log(err);
       setIsAddLoading(false);
@@ -180,7 +182,7 @@ const UserAddress = () => {
         "userData",
         JSON.stringify(response?.data?.response)
       );
-      swal("Deleted successfully", "", "success");
+      toast.success("🔥Address deleted successfully.");
     } catch (err) {
       if (err) {
         swal("something went wrong", "", "error");
@@ -274,7 +276,7 @@ const UserAddress = () => {
             >
               add new
             </button>
-            {showModal ? (
+            {showModal && (
               <>
                 <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                   <div className="relative my-6 mx-auto w-auto md:w-1/3">
@@ -393,7 +395,6 @@ const UserAddress = () => {
                               : "hover:bg-white hover:text-black"
                           } w-full bg-black text-white p-2 rounded-lg hover:border duration-200 border border-gray-300`}
                           onClick={(e) => {
-                            setShowModal(false);
                             handleSubmitForAddress(e);
                           }}
                         >
@@ -409,7 +410,7 @@ const UserAddress = () => {
                 </div>
                 <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
               </>
-            ) : null}
+            )}
           </div>
         </div>
       </div>
--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -9,6 +9,7 @@ import { HiOutlineLocationMarker } from "react-icons/hi";
 import { cartData } from "../redux/cart/cartSlice";
 import { toast } from "react-toastify";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
+import InlineButtonLoader from "../components/InlineButtonLoader";
 
 const CheckoutPage = () => {
   const [showModal, setShowModal] = useState(false);
@@ -21,6 +22,8 @@ const CheckoutPage = () => {
   const [pincode, setPincode] = useState("");
   const [pincodeError, setPincodeError] = useState("");
   const [selectedAddress, setSelectedAddress] = useState("");
+  const [isValidAdd, setIsValidAdd] = useState(false);
+  const [isAddLoading, setIsAddLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -28,6 +31,31 @@ const CheckoutPage = () => {
 
   const user = useSelector((state) => state.userData.user);
   const cart = useSelector((state) => state.cartData.cart);
+  useEffect(() => {
+    if (
+      address &&
+      city &&
+      state &&
+      pincode &&
+      addressError.length === 0 &&
+      cityError.length === 0 &&
+      stateError.length === 0 &&
+      pincodeError.length === 0
+    ) {
+      setIsValidAdd(false);
+    } else {
+      setIsValidAdd(true);
+    }
+  }, [
+    address,
+    city,
+    state,
+    pincode,
+    addressError,
+    cityError,
+    stateError,
+    pincodeError,
+  ]);
   const handleDelete = (ids) => {
     swal({
       title: "Are you sure! you want to delete this address?",
@@ -131,6 +159,8 @@ const CheckoutPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsAddLoading(true);
+
     try {
       const response = await axios.post(
         `${process.env.REACT_APP_BASEURL}/user/addAddress`,
@@ -149,11 +179,27 @@ const CheckoutPage = () => {
         JSON.stringify(response?.data?.response)
       );
       toast.success("🔥Address saved successfully.");
+      setaddress("");
+      setCity("");
+      setState("");
+      setPincode("");
+      setIsAddLoading(false);
+      setShowModal(false);
     } catch (err) {
       console.log(err);
+      setIsAddLoading(false);
     }
   };
-
+  const clearAddress = () => {
+    setaddress("");
+    setCity("");
+    setState("");
+    setPincode("");
+    setAddressError("");
+    setCityError("");
+    setStateError("");
+    setPincodeError("");
+  };
   const handlePayment = async () => {
     try {
       const response = await axios.post(
@@ -243,7 +289,7 @@ const CheckoutPage = () => {
                               currency="INR"
                             >
                               <button
-                                className="text-center hover:bg-black text-black hover:text-white p-2 px-5 rounded-2xl uppercase duration-200 border border-black"
+                                className="text-center hover:bg-black text-black hover:text-white p-2 px-5 rounded-xl uppercase duration-200 border border-black"
                                 onClick={() => {
                                   setSelectedAddress(address._id);
                                 }}
@@ -283,20 +329,37 @@ const CheckoutPage = () => {
           </div>
         </div>
       </div>
-      {showModal ? (
+
+      {showModal && (
         <>
-          <div className="justify-center items-center flex  fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative my-6 mx-auto w-auto md:w-1/3">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-xl font-semibold">Add New Address</h3>
+                <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                  <h3 className="text-xl font-semibold text-gray-900">
+                    Add New Address
+                  </h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
+                    type="button"
+                    onClick={() => {
+                      setShowModal(false);
+                      clearAddress();
+                    }}
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
                   >
-                    <span className=" text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      X
-                    </span>
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
                   </button>
                 </div>
                 <div className="relative p-6 flex-auto space-x-4">
@@ -313,7 +376,7 @@ const CheckoutPage = () => {
                         onBlur={handleAddress}
                         onChange={handleAddress}
                         placeholder="Enter your address"
-                        className="px-3 py-3 resize-none placeholder-slate-300 text-slate-600 relative  bg-white rounded text-sm border border-slate-300 outline-none focus:outline-none focus:ring w-full pl-10"
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 pl-10"
                       ></textarea>
                     </div>
                     <span className="text-red-500 text-sm">{addressError}</span>
@@ -329,7 +392,7 @@ const CheckoutPage = () => {
                         onChange={handleCity}
                         id="city"
                         placeholder="Your city name"
-                        className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border border-slate-300 outline-none focus:outline-none focus:ring w-full pl-10"
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 pl-10"
                       />
                     </div>
                     <span className="text-red-500 text-sm">{cityError}</span>
@@ -344,7 +407,7 @@ const CheckoutPage = () => {
                         placeholder="Your state name"
                         onBlur={handleState}
                         onChange={handleState}
-                        className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border border-slate-300 outline-none focus:outline-none focus:ring w-full pl-10"
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 pl-10"
                       />
                     </div>
                     <span className="text-red-500 text-sm">{stateError}</span>
@@ -354,13 +417,14 @@ const CheckoutPage = () => {
                         <i className="fas fa-location"></i>
                       </span>
                       <input
-                        type="number"
+                        type="text"
+                        maxLength={6}
                         value={pincode}
                         onBlur={handlePincode}
                         onChange={handlePincode}
                         placeholder="Enter picode"
                         id="pincode"
-                        className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border border-slate-300 outline-none focus:outline-none focus:ring w-full pl-10"
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 pl-10"
                       />
                     </div>
                     <span className="text-red-500 text-sm">{pincodeError}</span>
@@ -369,21 +433,18 @@ const CheckoutPage = () => {
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
+                    disabled={isValidAdd}
+                    className={`${
+                      isValidAdd
+                        ? "bg-black"
+                        : "hover:bg-white hover:text-black"
+                    } w-full bg-black text-white p-2 rounded-lg hover:border duration-200 border border-gray-300`}
                     onClick={(e) => {
-                      setShowModal(false);
                       handleSubmit(e);
                     }}
                   >
-                    Save Address
+                    {isAddLoading ? <InlineButtonLoader /> : "Save Address"}
                   </button>
                 </div>
               </div>
@@ -391,7 +452,7 @@ const CheckoutPage = () => {
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
-      ) : null}
+      )}
     </>
   );
 };
