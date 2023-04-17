0249a2161400d4475bd3ee0cab83c6d8e0baa9d9
Rohit kajavadra <krishnakajavadar6178@gmail.com>
2023-04-17 21:46:36 +0530

    category fetch only active fixed

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -4,6 +4,7 @@ const categoryController = require('../controller/categoryController');
 
 router.post('/add', categoryController.addCategory);
 router.get('/all', categoryController.fetchAllCategory);
+router.get('/allAdmin', categoryController.fetchAllCategoryAdmin);
 router.delete('/delete',categoryController.deleteCategory);
 router.put('/edit',categoryController.editCategory);
 
