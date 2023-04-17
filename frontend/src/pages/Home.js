c06b8c831353bf3bc992a725b525a739678e8b04
Rohit kajavadra <krishnakajavadar6178@gmail.com>
2023-04-17 20:59:14 +0530

    Navbar issue fixed in frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,64 +1,74 @@
-import axios from 'axios'
-import React from 'react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import { userData } from '../redux/user/userSlice'
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { userData, userLogIn } from "../redux/user/userSlice";
 
 import { cartData } from "../redux/cart/cartSlice";
-import MainNav from '../components/Navbar'
+import MainNav from "../components/Nav/MainNav";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const isUser = useSelector(state => state.userData.user);
-
+  const isUser = useSelector((state) => state.userData.user);
 
   useEffect(() => {
     if (isUser != null) {
       (async () => {
         try {
-          const response = await axios.get(`${process.env.REACT_APP_BASEURL}/cart/${isUser._id}`);
-          dispatch(cartData(response.data.data.cart))
+          const response = await axios.get(
+            `${process.env.REACT_APP_BASEURL}/cart/${isUser._id}`
+          );
+          dispatch(cartData(response.data.data.cart));
         } catch (err) {
           console.log(err);
         }
-      })()
+      })();
     }
-  }, [isUser])
-
+  }, [isUser]);
 
   useEffect(() => {
-    
     (async () => {
       try {
-        
-        const data = await axios.get(`${process.env.REACT_APP_BASEURL}/auth/login/success`, { withCredentials: true });
-        console.log('this is something');
-        console.log(data)     
-        if (data.status === 201 ) {
-          const isExist = await axios.get(`${process.env.REACT_APP_BASEURL}/user/isExist`, { withCredentials: true })
+        const data = await axios.get(
+          `${process.env.REACT_APP_BASEURL}/auth/login/success`,
+          { withCredentials: true }
+        );
+        console.log("this is something", data);
+        if (data.status === 201) {
+          const isExist = await axios.get(
+            `${process.env.REACT_APP_BASEURL}/user/isExist`,
+            { withCredentials: true }
+          );
           console.log(isExist + "check this exist");
-          dispatch(userData(isExist.data.user))
+          dispatch(userData(isExist?.data?.user));
+
+          dispatch(userLogIn(true));
+          localStorage.setItem("isUserLogIn", JSON.stringify(true));
+          localStorage.setItem("userData", JSON.stringify(isExist?.data?.user));
         } else {
-          dispatch(userData(data.data.user))
+          dispatch(userData(data?.data?.user));
+          dispatch(userLogIn(true));
+          localStorage.setItem("isUserLogIn", JSON.stringify(true));
+          localStorage.setItem("userData", JSON.stringify(data?.data?.user));
         }
       } catch (e) {
         console.log(e);
       }
-     })()
-  },[])
+    })();
+  }, []);
 
   return (
     <>
-    <div className='bg-orange-100'>
-      <MainNav />
-      <Outlet />
-      <Footer />
-    </div>
+      <div className="bg-orange-100">
+        <MainNav />
+        <Outlet />
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
