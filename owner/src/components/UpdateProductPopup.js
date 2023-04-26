634f05d71de9fb08106e9391d9b53169b2a27b50
poojansakhreliya8050 <poojasakhreliya12@gmail.com>
2023-04-26 19:30:01 +0530

    jk

--- owner/src/components/UpdateProductPopup.js
+++ owner/src/components/UpdateProductPopup.js
@@ -211,7 +211,7 @@ const UpdateProductPopup = ({
                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pl-10"
                   ></textarea>
                 </div>
-                <div className="relative w-full my-3">
+                {/* <div className="relative w-full my-3">
                   <label
                     className="block capitalize text-blueGray-600 text-md pb-1"
                     htmlFor="pdImage"
@@ -219,7 +219,7 @@ const UpdateProductPopup = ({
                     Upload prodouct image
                   </label>
                   <input type="file" name="productImage" id="pdImage" />
-                </div>
+                </div> */}
               </form>
             </div>
             {/*footer*/}
